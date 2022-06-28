3d671d9a593fc64299888d8845794e6767a980d5
Avrigeanu Sebastian <sand.avrigeanu@yahoo.com>
2022-06-28 14:32:17 +0300

    Data types inserted

--- Butterflysolver/MainWindow.xaml.cs
+++ Butterflysolver/MainWindow.xaml.cs
@@ -22,16 +22,29 @@ namespace Butterflysolver
     /// 
     public partial class MainWindow : Window
     {
-        private float FirstStepSum(float input)
-        {
-            float result;
-            return result;
-        }
+        double x0, x1, x2, x3, x4, x5, x6, x7;
+        Complex jplus = new Complex(0, 1);
+        Complex jminus = new Complex(0, -1);
+        double zero = 0;
+        double one = 1;
+        
         public MainWindow()
         {
             
-            InitializeComponent();
-            
+            InitializeComponent();       
+        }
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            x0 = float.Parse(input0.Text);
+            x1 = float.Parse(input1.Text);
+            x2 = float.Parse(input2.Text);
+            x3 = float.Parse(input3.Text);
+            x4 = float.Parse(input4.Text);
+            x5 = float.Parse(input5.Text);
+            x6 = float.Parse(input6.Text);
+            x7 = float.Parse(input7.Text);
         }
     }
 }
